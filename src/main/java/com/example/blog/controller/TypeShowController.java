efb447a2842668d72517b5bc96aa46f90888b0f1
sky <428933537@qq.com>
2020-05-30 17:13:20 +0800

    最后调整

--- src/main/java/com/example/blog/controller/TypeShowController.java
+++ src/main/java/com/example/blog/controller/TypeShowController.java
@@ -14,7 +14,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import sun.rmi.runtime.Log;
 
 import java.util.List;
 
